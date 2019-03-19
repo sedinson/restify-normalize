47f462bce8a82d6e5c3fd298d0288ec77ceca0ba
Edinson Salas <edinsonsalas@gmail.com>
2019-03-19 15:44:43 -0500

    Some improvements for

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 'use strict';
 
 var _ = require('lodash'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
+    async = require('async');
+
+var models = {};
 
 module.exports = function (knex) {
     return function (model, data) {
         return new Promise(function (resolve, reject) {
-            knex.raw(`describe ${model}`).then(function (response) {
-                //-- Buscar si hay algún dato que su nombre empiece por "_" y no este en el objeto de datos
-                _.each(response[0], function (row) {
-                    var field = row.Field;
-                    if(/^_/gi.test(field) && !data[field] && data[field.substr(1)]) {
-                        data[field] = data[field.substr(1)];
+            async.waterfall([
+                function (cb) {
+                    if(models[model]) {
+                        return cb(null, models[model]);
                     }
-                });
-    
-                data = _.pick(data || {}, _.map(response[0], 'Field'));
+
+                    knex.raw(`describe ${model}`).then(function (response) {
+                        models[model] = response[0];
+                        cb(null, data);
+                    }).catch(cb);
+                },
+
+                function (rows, cb) {
+                    //-- Buscar si hay algún dato que su nombre empiece por "_" y no este en el objeto de datos
+                    _.each(rows, function (row) {
+                        var field = row.Field;
+                        if(/^_/gi.test(field) && !data[field] && data[field.substr(1)]) {
+                            data[field] = data[field.substr(1)];
+                        }
+                    });
         
-                for (var name in data) {
-                    if(['null', 'undefined', undefined, null].indexOf(data[name]) >= 0) {
-                        delete data[name];
-                    } else {
-                        //-- Si es un json, entonces convertir a texto
-                        try {
-                            var info = data[name];
-                            data[name] = (info && typeof info == "object")? JSON.stringify(info) : info;
-                        } catch (e) { }
+                    data = _.pick(data || {}, _.map(rows, 'Field'));
+
+                    for (var name in data) {
+                        if(['null', 'undefined', undefined, null].indexOf(data[name]) >= 0) {
+                            delete data[name];
+                        } else {
+                            //-- Si es un json, entonces convertir a texto
+                            try {
+                                var info = data[name];
+                                data[name] = (info && typeof info == "object")? JSON.stringify(info) : info;
+                            } catch (e) { }
+                        }
                     }
+
+                    cb(null, data);
                 }
-        
+            ], function (err, data) {
+                if(err) {
+                    return reject(err);
+                }
+
                 resolve(data);
-            }).catch(reject);
+            });
         });
     };
 };
